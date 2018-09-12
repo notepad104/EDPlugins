675ba0f8ac141099f2b4c6deb0697a3772381aca
TiagoDanin <TiagoDanin@outlook.com>
2018-09-12 17:47:54 -0300

    Add processs error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 const debug = require('debug')
-//const gscomplet = require('gsearch')
+const stringify = require('json-stringify-safe')
 
 var webhookReply = process.env.webhook_reply == 'true' ? true : false
 const isWebhook = process.env.webhook == 'true' ? true : false
@@ -17,7 +17,6 @@ const bot = new Telegraf(process.env.telegram_token, telegrafOption)
 if (!isWebhook) {
 	webhookReply = false
 }
-
 if (!isTest) {
 	bot.telegram.deleteWebhook()
 }
@@ -31,15 +30,8 @@ const dlogInline = debug("bot:inline")
 const dlogCallback = debug("bot:callback")
 const dlogError = debug("bot:error")
 
-/*function autocomplent(query) {
-	console.log(`Input: ${query}`)
-	gscomplet.suggest('query', function(error, data, res) {
-		console.log('Autocomplent', data);
-	});
-}*/
-
 dlogBot("Start bot")
-var startLog = `
+const startLog = `
 *BOT INICIADO*
 *Modo de recebimento*: ${isWebhook ? 'webhook' : 'polling'}
 *Mode de retorno*: ${webhookReply ? 'Resposta ao webhook': 'normal'}
@@ -64,18 +56,97 @@ const plugins = [
 	'gif',
 	'github',
 	'help',
-	//'google', TODO: Add Test
-	//'ip', TODO: InProgress
+	//'google', //TODO: Add Test
+	'ip',
 	'latex',
 	'lmgtfy',
 	'perguntas',
 	'ping',
 	'soteio',
 	'text',
-	//'torrent', TODO: Check
+	//'torrent', //TODO: Check
 	'xkcd'
 ]
 
+function processError(error, ctx, plugin) {
+	var fulllog = []
+	var logId = `${+ new Date()}_`
+	if (ctx && ctx.update && ctx.update.update_id) {
+		logId += `${ctx.update.update_id}`
+	} else {
+		logId += 'NoUpdate'
+	}
+
+	var errorMsg = `
+*Ocorreu um erro* ao executar este comando.
+Se o problema persistir contante a equipe de suporte via \`/suporte sua mensagem.\`
+Entre no Canal @RoboED para ficar por dentro das atualizações do nosso querido Robô de Mimetal.
+\`ID:${logId}\`
+`
+	if (ctx && ctx.updateType) {
+		if (ctx.updateType == 'message') {
+			ctx.replyWithMarkdown(errorMsg)
+		} else if (ctx.updateType == 'callback_query' || ctx.updateType == 'edited_message') {
+			ctx.editMessageText(errorMsg, {
+				parse_mode: 'Markdown'
+			})
+		} else if (ctx.updateType == '') {
+			ctx.answerCbQuery(
+				errorMsg.replace(/\*/g, '').replace(/`/g, ''),
+				true
+			)
+		}
+	}
+
+	if (error) {
+		fulllog.push({type: 'error', data: error})
+		dlogError(`Oooops ${error}`)
+	}
+	if (ctx) {
+		fulllog.push({type: 'ctx', data: ctx})
+	}
+	if (plugin) {
+		fulllog.push({type: 'plugin', data: plugin})
+	}
+
+	var clearUser = (user) => JSON.stringify(user).replace(/[{"}]/g, '').replace(/,/g, '\n').replace(/:/g, ': ')
+
+	var text = `#Error ID:${logId}`
+	if (plugin && plugin.id) {
+		text += `\nPlugin ~> ${plugin.id}`
+	}
+	if (error) {
+		text += `\nERROR ~>\n${error.toString()}\n`
+	}
+	if (ctx && ctx.from) {
+		text += `\nFROM ~>\n${clearUser(ctx.from)}\n`
+	}
+	if (ctx && ctx.chat) {
+		text += `\nCHAT ~>\n${clearUser(ctx.chat)}`
+	}
+
+	bot.telegram.sendMessage(process.env.log_chat, text.substring(0,4000))
+
+	var jsonData = stringify(fulllog)
+	var remove = (name) => {
+		jsonData = jsonData.replace(new RegExp(name, 'gi'), 'OPS_SECRET')
+	}
+	[
+		process.env.telegram_token,
+		process.env.giphy_token,
+		process.env.github_token,
+		process.env.host,
+		process.env.secret_path,
+		process.env.port
+	].forEach(name => remove(name))
+	return bot.telegram.sendDocument(
+		process.env.log_chat,
+		{
+			filename: `${logId}.log.JSON`,
+			source: Buffer.from(jsonData, 'utf8')
+		}
+	)
+}
 
 var inline = []
 var callback = []
@@ -105,7 +176,7 @@ plugins.forEach(p => {
 	if (_.plugin) {
 		bot.hears(_.regex, async (ctx) => {
 			dlogPlugins(`Runnig cmd plugin: ${_.name}`)
-			await _.plugin(ctx)
+			await _.plugin(ctx).catch((e) => processError(e, ctx, _))
 		})
 	}
 
@@ -129,7 +200,7 @@ bot.on('inline_query', async (ctx) => {
 			isFound = true
 			ctx.match = match
 			dlogInline(`Runnig inline plugin: ${_.name}`)
-			await _.inline(ctx)
+			await _.inline(ctx).catch((e) => processError(e, ctx, _))
 		}
 	}
 	if (!isFound) {
@@ -153,7 +224,7 @@ bot.on('callback_query', async (ctx) => {
 			if (data.startsWith(_.id)) {
 				ctx.match = [].concat(data, data.split(':'))
 				dlogCallback(`Runnig callback plugin: ${_.name}`)
-				await _.callback(ctx)
+				await _.callback(ctx).catch((e) => processError(e, ctx, _))
 			}
 		}
 	}
